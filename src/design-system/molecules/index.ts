5049b80b090fa48f64c87fd4e65c2d21e0e690b4
Dan Rozén <dan.rozen@bjornlunden.com>
2025-07-26 19:39:15 +0200

    Add src/design-system/molecules/index.ts

---
+++ src/design-system/molecules/index.ts
@@ -0,0 +1,19 @@
+
+// Perfect barrel export for molecular components
+// Organized by component type for AI readability
+
+// Interactive components
+export * from './LoadingSpinner';
+export * from './StatusIndicator';
+
+// Content components
+export * from './EmptyState';
+
+// Animation components
+export * from './AnimatedReveal';
+export * from './StaggeredList';
+export * from './MorphingContainer';
+export * from './PurposefulFade';
+
+// Future molecular components will be added here
+// Following the pattern: export * from './ComponentName';
