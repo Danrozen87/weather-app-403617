2c0550455af08b65699b1edb0160ea50b9d54fd1
Dan Rozén <dan.rozen@bjornlunden.com>
2025-07-26 19:38:55 +0200

    Add src/design-system/atoms/index.ts

---
+++ src/design-system/atoms/index.ts
@@ -0,0 +1,15 @@
+
+// Perfect barrel export for atomic components
+// Organized by component type for AI readability
+
+// Layout components
+export * from './Box';
+export * from './Stack';
+export * from './Container';
+export * from './Section';
+
+// Typography components
+export * from './Typography';
+
+// Future atomic components will be added here
+// Following the pattern: export * from './ComponentName';
